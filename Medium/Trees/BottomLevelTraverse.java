6793c2d388478a6b749e539bd4182cd3c88ed17b
shrey200634 <dshrey60@gmail.com>
2026-01-29 01:31:13 +0530

    2 new question on trees

---
+++ Medium/Trees/BottomLevelTraverse.java
@@ -0,0 +1,64 @@
+package Medium.Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BottomLevelTraverse {
+    
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {  
+      }
+      TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+//sol
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+         List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            LinkedList<Integer> currentLevel = new LinkedList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+
+                if (reverse) {
+                    currentLevel.addFirst(currentNode.val);
+                } else {
+                    currentLevel.add(currentNode.val);
+                }
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            result.add(0,currentLevel);
+        }
+
+        return result;
+        
+    }
+}
+    
+
