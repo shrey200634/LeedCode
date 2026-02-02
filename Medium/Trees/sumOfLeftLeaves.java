c1d5eb1e4b5815e78113ca23dab21d54b4e616c2
shrey200634 <dshrey60@gmail.com>
2026-02-02 23:08:27 +0530

    add new ques

---
+++ Medium/Trees/sumOfLeftLeaves.java
@@ -0,0 +1,40 @@
+package Medium.Trees;
+
+import java.util.Stack;
+
+public class sumOfLeftLeaves {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+          this.right = right;
+      }
+  }
+   public int sumOfLeftLeaves(TreeNode root) {
+    if(root == null) return 0;
+    int ans = 0;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.push(root);
+    
+    while(!stack.empty()) {
+        TreeNode node = stack.pop();
+        if(node.left != null) {
+            if (node.left.left == null && node.left.right == null)
+                ans += node.left.val;
+            else
+                stack.push(node.left);
+        }
+        if(node.right != null) {
+            if (node.right.left != null || node.right.right != null)
+                stack.push(node.right);
+        }
+    }
+    return ans;
+}
+    
+}
