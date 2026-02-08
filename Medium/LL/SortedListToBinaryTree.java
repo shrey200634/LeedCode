3b18bc0b6cddfb0d194cd2cabeebc88979c28b17
shrey200634 <dshrey60@gmail.com>
2026-02-09 01:09:01 +0530

    add new ques on LL

---
+++ Medium/LL/SortedListToBinaryTree.java
@@ -0,0 +1,60 @@
+package Medium.LL;
+
+public class SortedListToBinaryTree {
+    public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ 
+    }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {  
+      }
+      TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+  public TreeNode sortedListToBST(ListNode head) {
+        if (head ==null){
+            return null;
+        }
+        if (head.next ==null){
+            return new TreeNode(head.val);
+        }
+        ListNode prev =null;
+        ListNode fast = head ;
+        ListNode slow = head ;
+
+        if (fast !=null&& fast .next !=null){
+           prev =slow ;
+           slow =slow.next ;
+           fast =fast.next.next ;
+
+
+        }
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        TreeNode root =new TreeNode(slow.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+
+        return root;
+
+
+
+        
+    }
+    
+}
