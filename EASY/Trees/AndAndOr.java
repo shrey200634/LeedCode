246cfbbe889e39fb5658d4f2a2b68e98c07e5ecf
shrey200634 <dshrey60@gmail.com>
2026-03-04 23:29:00 +0530

    addd bitwise and tree ques

---
+++ EASY/Trees/AndAndOr.java
@@ -0,0 +1,27 @@
+package EASY.Trees;
+
+public class AndAndOr {
+     public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+     public boolean evaluateTree(TreeNode root) {
+        if(root.left == null && root.right == null){
+            if(root.val == 0) return false;
+            return true;
+        }
+        boolean left = evaluateTree(root.left);
+        boolean right = evaluateTree(root.right);
+        if(root.val == 2) return left || right;
+        return left && right; 
+    }
+    
+}
