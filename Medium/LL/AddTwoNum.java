91a871ec7094a060add316982001641e6027b73b
shrey200634 <dshrey60@gmail.com>
2026-03-24 00:46:11 +0530

    add Two num ques

---
+++ Medium/LL/AddTwoNum.java
@@ -0,0 +1,49 @@
+package Medium.LL;
+
+public class AddTwoNum {
+    public class DeleteMiddleNode {
+    public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ 
+    }
+     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        
+        return reverseList(dummy.next);
+    }
+    
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+    
+    }}
