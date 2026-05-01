9462de039da81ccd9d3aa8df8880e90688aae9cb
shrey200634 <dshrey60@gmail.com>
2026-05-02 00:20:01 +0530

    added new ques

--- Medium/ques.java
+++ Medium/ques.java
@@ -1,5 +1,11 @@
 package Medium;
 
 public class ques {
-    
+
+    public boolean isPowerOfThree(int n) {
+        int maxPowerOf3 = 1162261467;
+        return n > 0 && maxPowerOf3 % n == 0;
+    }
 }
+    
+
